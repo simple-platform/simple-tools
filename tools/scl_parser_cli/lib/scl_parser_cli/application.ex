a9e60697fce386a576797e8a70b0d82722d6e35b
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2026-01-12 14:34:59 +0530

    refactor(scl-parser-cli): update logic to resolve binary execution issue (#43)

--- tools/scl_parser_cli/lib/scl_parser_cli/application.ex
+++ tools/scl_parser_cli/lib/scl_parser_cli/application.ex
@@ -4,29 +4,8 @@ defmodule SCLParserCLI.Application do
 
   @impl true
   def start(_type, _args) do
-    # Runtime Check for Test Environment
-    # If ExUnit is loaded, we are testing. Start Supervisor.
-    # If ExUnit is NOT loaded, we are a Release. Run Script & Halt.
-    if Code.ensure_loaded?(ExUnit) do
-      children = []
-      opts = [strategy: :one_for_one, name: SCLParserCLI.Supervisor]
-      Supervisor.start_link(children, opts)
-    else
-      # Burrito CLI Mode
-
-      # 1. Fetch Arguments safely
-      argv =
-        if Code.ensure_loaded?(Burrito.Util.Args) do
-          Burrito.Util.Args.get_arguments()
-        else
-          :init.get_plain_arguments() |> Enum.map(&List.to_string/1)
-        end
-
-      # 2. Execute Main Logic
-      SCLParserCLI.main(argv)
-
-      # 3. Explicit Halt (if main hasn't already halted)
-      System.halt(0)
-    end
+    argv = Burrito.Util.Args.get_arguments()
+    SCLParserCLI.main(argv)
+    System.halt(0)
   end
 end
